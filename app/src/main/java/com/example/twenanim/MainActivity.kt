9ec850e726ec9c8b24bbe3edafaa63c4e3fe05a9
Almond1400 <mpcknna@gmail.com>
2024-11-28 16:40:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/twenanim/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.twenanim
+
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //Обьявление imageView
+        val imgCat : ImageView = findViewById(R.id.ivAnimFox)
+
+        val anim : Animation =
+            AnimationUtils.loadAnimation(this,R.anim.new_animation)
+        imgCat.startAnimation(anim)
+    }
+}
